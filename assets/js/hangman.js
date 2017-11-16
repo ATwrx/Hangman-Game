70a99ae23763de84e715ef8ceee6d7b14b6ca881
A. Thompson <act.webdesigns@gmail.com>
2017-11-16 15:35:05 -0600

    creating hangman game as an object

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,20 +1,39 @@
 $(document).ready(function() {
 
-  $(".hide-me").click(function() {
-    // Hides the instructions after the first key press
-      $(this).hide(1000);
-      $("#hide").hide(1000);
-      console.log("I hear ya");
-  });
+  var words = [ "elves", "santa", "rudolph", "presents", "winter", "snow" ];
 
-  var words = [
-    "elves",
-    "santa",
-    "rudolph",
-    "presents",
-    "winter",
-    "snow"
-  ];
-  var guesses = 5; // nd. turn into counter  function
-  var wins = 0; // nd. turn into counter function
+  var hangman = {
+    correctWords: 0,
+    guesses: 6,
+    currentWord: new String();
+    wrongLetters: new Array();
+    startGame: function() {
+
+    }
+  }
+  // Loops through words
+  for (i = 0; i < words.length; i++) {
+    //splits words[i] into an array of strings
+    var wordStrArr = words[i].split("");
+    var
+
+
+    }
 });
+ //    ##Snippet from class##
+ // document.onkeyup = function(event) {
+ //        var key = event.keyCode;
+ //        if (key == 72) {
+ //          car.honk();
+ //        } else if (key == 87) {
+ //          car.driveAroundWorld();
+ //        } else if (key == 68){
+ //          car.driveToWork();
+ //        } else if (key == 84)
+ //         car.getTuneUp();
+ //        } else {
+ //        console.log('Try a different key.')
+ //      };
+
+
+// obj?
